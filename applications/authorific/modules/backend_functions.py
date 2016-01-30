4882ffde90afb3b29db7070cf2ab6e10f24785b6
Max Conway <conway.max1@gmail.com>
2016-01-30 23:58:59 +0000

    First attempt at reading a model

--- applications/authorific/modules/backend_functions.py
+++ applications/authorific/modules/backend_functions.py
@@ -1,2 +1,17 @@
+import os
+import csv
+import json
+import pickle
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+
 def callme(text):
+
+	x = pickle.load(open("./moduledata/svm_classifier.p", "rb"))
+
+	predicted_svm2 = x.predict(test_tfidf) 
+	print predicted_svm2
+
 	return(text)
