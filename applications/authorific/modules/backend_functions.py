79363975a8cf5b60eb20060a788dabb826450a0c
Max Conway <conway.max1@gmail.com>
2016-01-31 00:39:51 +0000

    traine

--- applications/authorific/modules/backend_functions.py
+++ applications/authorific/modules/backend_functions.py
@@ -6,12 +6,18 @@ import pickle
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+tfidf_transformer = TfidfTransformer(use_idf=True, smooth_idf=True)
 
 def callme(text):
 
-	x = pickle.load(open("./moduledata/svm_classifier.p", "rb"))
-
-	predicted_svm2 = x.predict(test_tfidf) 
+	# mod = pickle.load(open("./moduledata/svm_classifier.p", "rb"))
+	# count_vect = pickle.load(open("./moduledata/count_vect.p", "rb"))
+	trained = pickle.load(open("./moduledata/all_trained.p", "rb"))
+	test_counts = count_vect.transform(pd.Series(['baby baby more baby']))
+	test_tfidf = tfidf_transformer.transform(test_counts)
+	predicted_svm2 = mod.predict(test_tfidf) 
 	print predicted_svm2
 
-	return(text)
+	return(predicted_svm2[0])
