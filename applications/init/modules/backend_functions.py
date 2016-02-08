b4c07928bcc1b9ba7847500cf4539d5cf388b094
Max Conway <conway.max1@gmail.com>
2016-02-08 20:53:52 +0000

    Updates for multiple classifications

--- applications/init/modules/backend_functions.py
+++ applications/init/modules/backend_functions.py
@@ -1,4 +1,4 @@
-from gluon.custom_import import track_changes; track_changes(True)
+# from gluon.custom_import import track_changes; track_changes(True)
 
 import os
 import csv
@@ -11,16 +11,32 @@ import pandas as pd
 # from sklearn.feature_extraction.text import CountVectorizer
 # from sklearn.feature_extraction.text import TfidfTransformer
 
-def callme(text):
+moduledir = os.path.dirname(os.path.abspath('__file__'))
+songs = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/songs_all_trained.p'), "rb"))
+literature = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/literature_all_trained.p'), "rb"))
+speeches = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/speeches_all_trained.p'), "rb"))
+# old_songs = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/all_trained.p'), "rb"))
 
-	moduledir = os.path.dirname(os.path.abspath('__file__'))
-	trained = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/all_trained.p'), "rb"))
-	count_vect = trained['music_count_vect']
-	mod = trained['music_classifier_svm_fit']
-	tfidf_transformer = trained['music_tfidf_transformer']
+
+def use_model(text, trained):
+	count_vect = trained['count_vect']
+	mod = trained['classifier_svm_fit']
+	tfidf_transformer = trained['tfidf_transformer']
 	test_counts = count_vect.transform(pd.Series([text]))
 	test_tfidf = tfidf_transformer.transform(test_counts)
 	predicted_svm2 = mod.predict(test_tfidf) 
-	print predicted_svm2
 
 	return(predicted_svm2[0])
+
+def predict_song(text):
+	return(use_model(text,songs))
+
+def predict_literature(text):
+	return(use_model(text,literature))
+
+def predict_speech(text):
+	return(use_model(text,speeches))
+
+def callme(text):
+	return(use_model(text,old_songs))
+
