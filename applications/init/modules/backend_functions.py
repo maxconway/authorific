9a93e9fdee37cfe54e618fcc3ccfc53df679fd98
Max Conway <conway.max1@gmail.com>
2016-02-09 00:25:32 +0000

    Revert "Re-added literature"
    This reverts commit 81f1c691a4f400b88bc0a5e2a7fc503ee6e77708.

--- applications/init/modules/backend_functions.py
+++ applications/init/modules/backend_functions.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 moduledir = os.path.dirname(os.path.abspath('__file__'))
 songs = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/songs_all_trained.p'), "rb"))
-literature = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/literature_all_trained.p'), "rb"))
+#literature = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/literature_all_trained.p'), "rb"))
 speeches = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/speeches_all_trained.p'), "rb"))
 # old_songs = pickle.load(open(os.path.join(moduledir, 'applications/init/modules/moduledata/all_trained.p'), "rb"))
 
